a129188505ef8a52dadae008974212fd1134db2a
Jiedara <m.villanove@laposte.net>
2015-10-22 14:09:17 +0200

    Nouvelle pallette couleurs contextuel (var.scss) + gutter(margin) panel + inset hovermenu module + dotted border hove rmodule

--- templates/guildPanel2015/js/board.js
+++ templates/guildPanel2015/js/board.js
@@ -2,27 +2,30 @@ $(document).ready(function () {
 	var modWidth = $('#main-board').width() - ($('#main-board').width() % 100);
 	$('.module-disclaimer').width(modWidth);
 	$('#main-board').width(modWidth);
-	var $board = $('#main-board').packery({
+	var board = $('#main-board').packery({
 		columnWidth: 100,
 		rowHeight: 100,
-		itemSelector: '.module'
+		itemSelector: '.module',
+		gutter:10,
 	});
 
-	$board.find('.module').each(function (i, itemElem) {
+	board.find('.module').each(function (i, itemElem) {
 		var draggie = new Draggabilly(itemElem);
-		$board.packery('bindDraggabillyEvents', draggie);
+		board.packery('bindDraggabillyEvents', draggie);
 	});
 
 	$('.module-option .fa-close').on('click',function(){
 		$(this).parents('.module').remove();
 	});
 	$('.module-option .fa-thumb-tack').on('click',function(){
-		$('#board').packery('stamp',$(this).parents('.module'));
+		console.log('fixé');
+		$(this).parents('.module').draggabilly('disable');
 		$(this).hide();
 		$(this).parent().children('.fa-arrows').show();
 	});
 	$('.module-option .fa-arrows').on('click',function(){
-		$('#board').packery('stamp',$(this).parents('.module'));
+		console.log('draggable');
+		$(this).parents('.module').draggabilly('enable');
 		$(this).hide();
 		$(this).parent().children('.fa-thumb-tack').show();
 	});
